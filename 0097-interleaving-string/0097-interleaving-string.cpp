5d9c942761d96c272f7141773f41c4ad97eb2a0b
aHZAMp <124917768+aHZAMp@users.noreply.github.com>
2023-08-25 11:35:54 +0530

    Time: 28 ms (31.09%), Space: 76.7 MB (14.58%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     
-    bool ways(int i,int j,int k,string &s1,string &s2,string &s3)
+    bool ways(int i,int j,int k,string &s1,string &s2,string &s3, vector<vector<vector<int> > >&dp)
     {
         if(i<0&&j<0&&k<0)return true;
         if(i<0&&j<0)return false;
@@ -34,14 +34,14 @@ public:
          else
              return false;
         }
-        
+        if(dp[i][j][k]!=-1)return dp[i][j][k];
         if(s1[i]==s3[k]&&s2[j]==s3[k])
-            return ways(i-1,j,k-1,s1,s2,s3)||ways(i,j-1,k-1,s1,s2,s3);
+            return dp[i][j][k]=ways(i-1,j,k-1,s1,s2,s3,dp)||ways(i,j-1,k-1,s1,s2,s3,dp);
         else if(s1[i]==s3[k]&&s2[j]!=s3[k])
-            return ways(i-1,j,k-1,s1,s2,s3);
+            return dp[i][j][k]=ways(i-1,j,k-1,s1,s2,s3,dp);
        else if(s2[j]==s3[k]&&s1[i]!=s3[k])
-           return ways(i,j-1,k-1,s1,s2,s3);
-        return false;
+           return dp[i][j][k]=ways(i,j-1,k-1,s1,s2,s3,dp);
+        return dp[i][j][k]=false;
     }
     
     bool isInterleave(string s1, string s2, string s3) {
@@ -50,6 +50,8 @@ public:
         int p=s3.size();
         if(n+m!=p)
             return false;
-        return ways(n-1,m-1,p-1,s1,s2,s3);
+         vector<vector<vector<int> > > dp(
+        n, vector<vector<int> >(m, vector<int>(p,-1)));
+        return ways(n-1,m-1,p-1,s1,s2,s3,dp);
     }
 };
