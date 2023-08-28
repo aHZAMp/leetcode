8a088c8c8f33d55e20db04e7efb827c6f6eb4716
aHZAMp <124917768+aHZAMp@users.noreply.github.com>
2023-08-28 10:51:27 +0530

    Time: 3 ms (36.88%), Space: 6.8 MB (67.22%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -0,0 +1,39 @@
+class MyStack {
+public:
+    deque<int>q;
+    
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+    q.push_back(x);    
+    }
+    
+    int pop() {
+        int a=q.back();
+        q.pop_back();
+        return a;
+    }
+    
+    int top() {
+        return q.back();
+        
+    }
+    
+    bool empty() {
+        if(q.empty())
+            return true;
+        else
+            return false;
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
