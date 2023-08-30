faa2c88db19aed0c2aac1715567c6e842e7a53f3
aHZAMp <124917768+aHZAMp@users.noreply.github.com>
2023-08-30 17:19:40 +0530

    Time: 55 ms (5.01%), Space: 31.3 MB (5.07%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,101 @@
+struct Node{
+
+    Node* links[26];
+    bool flag=false;
+    bool containsKey(char ch)
+    {
+        return (links[ch-'a']!=NULL);
+    }
+    void put(char ch,Node* node)
+    {
+        links[ch-'a']=node;
+    }
+    Node* get(char ch)
+    {
+        return links[ch-'a'];
+    }
+    void setEnd()
+    {
+        flag=true;
+    }
+    bool isEnd()
+    {
+        return flag;
+    }
+};
+
+class Trie {
+
+
+  
+
+public:
+
+    /** Initialize your data structure here. */
+    Node* root;
+    Trie() {
+       root=new Node();
+    }
+
+    /** Inserts a word into the trie. */
+    void insert(string word) {
+         Node* node=root;
+         for(int i=0;i<word.size();i++)
+         {
+             if(!node->containsKey(word[i]))
+                node->put(word[i], new Node());
+
+              node=node->get(word[i]);  
+         }     
+          node->setEnd();
+
+
+    }
+
+    /** Returns if the word is in the trie. */
+    bool search(string word) {
+          Node* node=root;
+          for(int i=0;i<word.size();i++)
+          {
+              if(!node->containsKey(word[i]))
+                  return false;
+               node=node->get(word[i]);   
+          }
+          return node->isEnd();
+    }
+};
+
+class Solution {
+public:
+      
+    
+    bool ways(int ind,int n,string &s,Trie* obj,vector<int>&dp)
+    {
+        if(ind==n)
+            return true;
+        if(dp[ind]!=-1)
+            return dp[ind];
+        string t="";bool res=false;
+        for(int i=ind;i<n;i++)
+        {
+            t=t+s[i];
+            if(obj->search(t)){
+                //t="";
+            res|=ways(i+1,n,s,obj,dp);
+            }
+        }
+        return dp[ind]=res;
+        
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        
+        Trie* obj=new Trie();
+        for(int i=0;i<wordDict.size();i++)
+        {
+            obj->insert(wordDict[i]);
+        }
+        int n=s.size();
+        vector<int>dp(n+1,-1);
+        return ways(0,s.size(),s,obj,dp);
+    }
+};
