5dfcadf70049a86905692d80a6a4a7d83e0ce7b0
aHZAMp <124917768+aHZAMp@users.noreply.github.com>
2023-09-09 15:33:52 +0530

    Time: 0 ms (100.00%), Space: 20.6 MB (16.24%) - LeetHub

--- 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.cpp
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.cpp
@@ -3,12 +3,27 @@ public:
     int countPairs(vector<int>& nums, int target) {
         
         int c=0;
-        for(int i=0;i<nums.size();i++)
+        // for(int i=0;i<nums.size();i++)
+        // {
+        //     for(int j=i+1;j<nums.size();j++)
+        //     {
+        //         if(nums[i]+nums[j]<target)
+        //             c++;
+        //     }
+        // }
+        sort(nums.begin(),nums.end());
+        int i=0,j=nums.size()-1;
+        while(i<j)
         {
-            for(int j=i+1;j<nums.size();j++)
+            if(nums[i]+nums[j]<target)
             {
-                if(nums[i]+nums[j]<target)
-                    c++;
+                c++;
+                c+=j-1-i;
+                i++;
+            }
+            else
+            {
+                j--;
             }
         }
         return c;
